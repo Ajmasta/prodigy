3b9a939e439cab469e117f8e5254a786190886d8
ajmasta <adrien.moevus@mail.mcgill.ca>
2020-08-28 11:01:45 -0400

    Updates

---
+++ precache-manifest.0e08c6816aab9ee8ec92ed209ed91216.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285c7a3133c5c0f1ee7ca998fbfb04ed",
+    "url": "/mindsetprodigy/index.html"
+  },
+  {
+    "revision": "4f66542d4f0924c64010",
+    "url": "/mindsetprodigy/static/css/2.21a773b6.chunk.css"
+  },
+  {
+    "revision": "d1d306398ac7772fbcd2",
+    "url": "/mindsetprodigy/static/css/main.2d5d4d40.chunk.css"
+  },
+  {
+    "revision": "4f66542d4f0924c64010",
+    "url": "/mindsetprodigy/static/js/2.3222eb27.chunk.js"
+  },
+  {
+    "revision": "4c39d8cf0543d486569d80812654879c",
+    "url": "/mindsetprodigy/static/js/2.3222eb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d306398ac7772fbcd2",
+    "url": "/mindsetprodigy/static/js/main.f6a34ea7.chunk.js"
+  },
+  {
+    "revision": "ce46314d45ea0962ccae",
+    "url": "/mindsetprodigy/static/js/runtime-main.b9dc75b0.js"
+  },
+  {
+    "revision": "04809c38f9e7a830e50810841e6202d4",
+    "url": "/mindsetprodigy/static/media/1.04809c38.jpg"
+  },
+  {
+    "revision": "011e848f2ba1986b9eceb0cea0032e00",
+    "url": "/mindsetprodigy/static/media/10.011e848f.jpg"
+  },
+  {
+    "revision": "a7b6040e4ade6b87cc63d31a60a6b2fc",
+    "url": "/mindsetprodigy/static/media/11.a7b6040e.jpg"
+  },
+  {
+    "revision": "8d38f37d37e78103c42711f6c9af1597",
+    "url": "/mindsetprodigy/static/media/12.8d38f37d.jpg"
+  },
+  {
+    "revision": "af9b79ecc83bf7ce6a58c54768ead3e1",
+    "url": "/mindsetprodigy/static/media/13.af9b79ec.jpg"
+  },
+  {
+    "revision": "3f12353b88455f5c566c5d7e97b8e1c7",
+    "url": "/mindsetprodigy/static/media/14.3f12353b.jpg"
+  },
+  {
+    "revision": "b29d4f5c0405b92f75f4cfcd08f8e132",
+    "url": "/mindsetprodigy/static/media/2.b29d4f5c.jpg"
+  },
+  {
+    "revision": "5ff4a7b8b6f4f7b4f1f6eda7100b50eb",
+    "url": "/mindsetprodigy/static/media/3.5ff4a7b8.jpg"
+  },
+  {
+    "revision": "54c0951d937895556654c0f239465247",
+    "url": "/mindsetprodigy/static/media/4.54c0951d.jpg"
+  },
+  {
+    "revision": "14fd6f1c556262578986c329953f49b2",
+    "url": "/mindsetprodigy/static/media/5.14fd6f1c.jpg"
+  },
+  {
+    "revision": "0c95ee233d33a60c09aeac6710b08956",
+    "url": "/mindsetprodigy/static/media/6.0c95ee23.jpg"
+  },
+  {
+    "revision": "b940e4e40d3dc7de531a4b31d60593db",
+    "url": "/mindsetprodigy/static/media/7.b940e4e4.jpg"
+  },
+  {
+    "revision": "bb35804a58f83f9d7fee0197ac9ccc41",
+    "url": "/mindsetprodigy/static/media/8.bb35804a.jpg"
+  },
+  {
+    "revision": "568bb2e956ff327af766376cc1f0a465",
+    "url": "/mindsetprodigy/static/media/9.568bb2e9.jpg"
+  },
+  {
+    "revision": "0d9296ad35ffa8cf35f89a79f311a3e0",
+    "url": "/mindsetprodigy/static/media/NoneShallPass-B598.0d9296ad.ttf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/mindsetprodigy/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "c1f81144897fc5acffc67de8febd9690",
+    "url": "/mindsetprodigy/static/media/Saiyan-Sans.c1f81144.ttf"
+  },
+  {
+    "revision": "ca65b211bca1dd2c6f225db8af9586c7",
+    "url": "/mindsetprodigy/static/media/SpriteGraffiti-Regular.ca65b211.ttf"
+  },
+  {
+    "revision": "781b10f2a1f6c8cc86e48dfa1c4f652c",
+    "url": "/mindsetprodigy/static/media/Super Webcomic Bros..781b10f2.ttf"
+  },
+  {
+    "revision": "6077f78c07cd6088f88cfb19d5dda427",
+    "url": "/mindsetprodigy/static/media/background.6077f78c.mp4"
+  },
+  {
+    "revision": "39e195ed165ada7d15820d41ae4b8347",
+    "url": "/mindsetprodigy/static/media/backgroundLogin.39e195ed.png"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/mindsetprodigy/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "59604ec6cd10152c91f23e661722710f",
+    "url": "/mindsetprodigy/static/media/music1.59604ec6.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music10.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music11.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music12.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music13.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music14.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music2.06552745.mp3"
+  },
+  {
+    "revision": "c4b484849ad4d74f2f1bef4c3975a916",
+    "url": "/mindsetprodigy/static/media/music3.c4b48484.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music4.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music5.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music6.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music7.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music8.06552745.mp3"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/music9.06552745.mp3"
+  }
+]);
